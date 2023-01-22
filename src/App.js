3988283510eb59f4525d4196f1f6494e706e846d
Gabriel Figueiredo <gabriel.devsistema@gmail.com>
2023-01-22 10:37:43 -0300

    installing and styling component with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { Navbar } from "react-bootstrap";
 import Questions from "./components/Questions/questions";
 
 function App() {
   return (
     <div>
+      <Navbar className="px-4" bg="dark" >
+          <Navbar className="text-white" href="#home">Perguntar.com</Navbar>
+      </Navbar>
       <Questions />
     </div>
   );
--- src/components/Questions/questions.jsx
+++ src/components/Questions/questions.jsx
@@ -1,16 +1,30 @@
+import { Button, Container } from "react-bootstrap";
+
 export default function Questions(params) {
-    return (
-        <div>
-            <div>
-                <h1>Faça sua pergunta</h1>
-            </div>
-            <div>
-                <input placeholder="Titulo" />
-            </div>
-            <label>Descrição</label>
-            <textarea>
-                descreva sua pergunta
+  return (
+    <div>
+      <Container>
+        <form class="d-grid gap-3">
+          <div>
+            <h1>Faça sua pergunta</h1>
+          </div>
+          <div>
+            <input placeholder="Titulo" className="form-control" />
+          </div>
+          <label>Descrição</label>
+          <div className="form-floating">
+            <textarea
+              placeholder="descrição da pergunta"
+              className="form-control"
+              style={{height: 300}}
+            >
             </textarea>
-        </div>
-    )
+          </div>
+          <div>
+            <Button variant="primary">Perguntar</Button>{" "}
+          </div>
+        </form>
+      </Container>
+    </div>
+  );
 }
