5f386ab7df4d85939a726dc23221000cb9f15328
Gabriel Figueiredo <gabriel.devsistema@gmail.com>
2023-01-23 10:39:26 -0300

    getting data and showing it on the frontend

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require('body-parser')
 const connection = require("./database/database");
 const questions = require("./database/Questions_model")
 
@@ -7,6 +8,8 @@ const app = express();
 
 app.use(cors())
 app.use(express.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json())
 
 connection.authenticate().then(() => {
     console.log('conexao com banco de dados');
@@ -14,19 +17,22 @@ connection.authenticate().then(() => {
     console.log(erro);
 })
 
-app.get("/", (req,res) => {
-    res.send("seja bem vindo")
+app.get("/", async (req,res) => {
+   const q = await questions.findAll({raw: true})
+        res.status(200).send(q)
 })
 
-app.post("/questions", (req,res) => {
+
+app.post("/questions", async (req,res) => {
     const title = req.body.title
     const description = req.body.description
 
-    questions.create({
+   const resposta = await questions.create({
         title: title,
         description: description
     }).then(() => {
         res.redirect('/');
+        res.send({resposta})
     })
 })
 
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,7 +1,40 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Button, Card, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 export default function List(params) {
-    return (
-        <div>
-            LIsta de perguntas
-        </div>
-    )
+  const [listQuestions, setListQuestions] = useState();
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/")
+      .then((res) => {
+        setListQuestions(res.data);
+      })
+      .catch((error) => {
+        console.log(error.res);
+      });
+  }, []);
+  console.log(listQuestions);
+  return (
+    <Container className="d-grid gap-3">
+      <h1>Lista de Perguntas</h1>
+      <Link to={"/questions"}>
+        <Button variant="primary">Perguntar</Button>
+      </Link>
+      {listQuestions?.map((list) => {
+        return (
+          <Card>
+            <div className="card-body">
+              <p>{list.description}</p>
+            </div>
+            <div className="card-footer">
+                <Button variant="primary">Responder</Button>
+            </div>
+          </Card>
+        );
+      })}
+    </Container>
+  );
 }
