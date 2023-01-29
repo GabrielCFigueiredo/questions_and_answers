debb2930a400a0a4073e16b40e470b8545b6e74f
Gabriel Figueiredo <gabriel.devsistema@gmail.com>
2023-01-29 18:36:17 -0300

    update

--- server/database/config/config.js
+++ server/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    HOST: "bt8ss7n5dkszdbuuj8yp-mysql.services.clever-cloud.com",
-    USER: "usbdy55ejbz8pmv6",
-    PASSWORD: 'bQaZmML5srlLsGhRC88S',
-    DATABASE: "bt8ss7n5dkszdbuuj8yp",
+    MYSQL_ADDON_HOST: "bt8ss7n5dkszdbuuj8yp-mysql.services.clever-cloud.com",
+    MYSQL_ADDON_USER: "usbdy55ejbz8pmv6",
+    MYSQL_ADDON_PASSWORD: 'bQaZmML5srlLsGhRC88S',
+    MYSQL_ADDON_DB: "bt8ss7n5dkszdbuuj8yp",
     DIALECT: "mysql"
 }
--- server/database/database.js
+++ server/database/database.js
@@ -2,8 +2,8 @@ const Sequelize  = require("sequelize");
 const dbConfig  = require("../database/config/config")
 
 
-const connection = new Sequelize(dbConfig.DATABASE, dbConfig.USER, dbConfig.PASSWORD, {
-    host: dbConfig.HOST,
+const connection = new Sequelize(dbConfig.MYSQL_ADDON_DB, dbConfig.MYSQL_ADDON_USER, dbConfig.MYSQL_ADDON_PASSWORD, {
+    host: dbConfig.MYSQL_ADDON_HOST,
     dialect: dbConfig.DIALECT,
     
 });
